86ca1294a0ac7860afd756cdd6ef15830be0df56
Võ Thái <100753020+vothaidubi2@users.noreply.github.com>
2023-10-30 18:52:35 +0700

    thai

--- src/utils/UsersAPI.js
+++ src/utils/UsersAPI.js
@@ -8,6 +8,10 @@ class UsersAPI {
             },
         })
     }
+    updateUser = async (url) => {
+        return await axios.put(process.env.REACT_APP_BASE_DOMAIN + url)
+        
+    }
     updateBalance = async (url) => {
         return await axios.put(process.env.REACT_APP_BASE_DOMAIN + url)
         .then(data => data.data)
